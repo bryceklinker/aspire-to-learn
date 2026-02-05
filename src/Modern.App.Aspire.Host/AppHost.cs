df9c0047a27e61ea8f08846b0af41a0af7aa90bb
Bryce Klinker <bryce.klinker@gmail.com>
2026-02-04 19:52:27 -0600

    trying to get dev cert to work

--- src/Modern.App.Aspire.Host/AppHost.cs
+++ src/Modern.App.Aspire.Host/AppHost.cs
@@ -3,10 +3,12 @@ using Modern.App.Aspire.Host;
 var builder = DistributedApplication.CreateBuilder(args);
 
 var usersApi = builder.AddProject<Projects.Modern_App_Users_Api_Host>(ModernAppResourceName.UsersApi)
-    .WithExternalHttpEndpoints();
+    .WithExternalHttpEndpoints()
+    .WithDeveloperCertificateTrust(trust: true);
 
 builder.AddProject<Projects.Modern_App_Users_Web_Host>(ModernAppResourceName.UsersWeb)
     .WithReference(usersApi)
-    .WithExternalHttpEndpoints();
+    .WithExternalHttpEndpoints()
+    .WithDeveloperCertificateTrust(trust: true);
 
 builder.Build().Run();
